e81e093b954be063efed7dba44578ed0e93eda67
Qiming <LuQiMing@aactechnologies.com>
2024-08-26 14:05:51 +0800

    uploaded MT-compiled libs

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 A test app to verify tesseract lib.
 
 Please follow the instructions in this document to build tesseract and leptonica by yourself.
-https://blog.csdn.net/happydeer
+https://blog.csdn.net/happydeer/article/details/141561569
--- tesseract_testapp.cpp
+++ tesseract_testapp.cpp
@@ -8,11 +8,13 @@
 int main()
 {
     tesseract::TessBaseAPI* api = new tesseract::TessBaseAPI();
+    // 初始化，加载语言包
     if (api->Init("tessdata", "eng")) {
         std::cout << "Could not initialize tesseract" << std::endl;
         return -1;
     }
 
+    // 因为我们编译的leptonica库支持的格式有限，此处用原生支持的BMP来测试
     Pix* image = pixRead("numbers.bmp");
     if (image) {
         api->SetImage(image);
