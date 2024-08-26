ed0bcd27fbec2b5361feeb430ab5736540a2da7d
Qiming <LuQiMing@aactechnologies.com>
2024-08-26 17:31:25 +0800

    测试中文语言包

--- tesseract_testapp.cpp
+++ tesseract_testapp.cpp
@@ -4,22 +4,47 @@
 #include "baseapi.h" 
 #include "allheaders.h"
 #include <iostream>
+#include <string>
+#include <windows.h>
+
+//#define _TEST_CHINESE_
+
+#ifdef _TEST_CHINESE_
+const char szLang[] = "chi_sim";
+const char szSourceFile[] = "chinese.bmp";
+#else
+const char szLang[] = "eng";
+const char szSourceFile[] = "numbers.bmp";
+#endif
 
 int main()
-{
+{  
     tesseract::TessBaseAPI* api = new tesseract::TessBaseAPI();
     // 初始化，加载语言包
-    if (api->Init("tessdata", "eng")) {
+    if (api->Init("tessdata", szLang)) {
         std::cout << "Could not initialize tesseract" << std::endl;
         return -1;
     }
 
     // 因为我们编译的leptonica库支持的格式有限，此处用原生支持的BMP来测试
-    Pix* image = pixRead("numbers.bmp");
+    Pix* image = pixRead(szSourceFile);
     if (image) {
         api->SetImage(image);
         char* outText = api->GetUTF8Text();
-        std::cout << "OCR result: " << outText << std::endl;
+
+#ifdef _TEST_CHINESE_
+        std::locale currentLocale = std::locale::global(std::locale(""));
+        std::wcout.imbue(std::locale());
+
+        int size = ::MultiByteToWideChar(CP_UTF8, 0, outText, -1, NULL, 0);
+        wchar_t* wcStr = new wchar_t[size];
+        MultiByteToWideChar(CP_UTF8, 0, outText, -1, wcStr, size);
+        std::wcout << L"OCR Result: " << wcStr << std::endl;
+        delete[] wcStr;
+#else
+        std::cout << "OCR Result: " << outText << std::endl;
+#endif // _TEST_CHINESE_
+        
         delete[] outText;
         pixDestroy(&image);
     }
